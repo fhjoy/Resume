d8be667774cf885d1b9014b8c636b8718e715d66
Md Faisal Hossain <faisalh.iu@gmail.com>
2022-03-17 22:16:54 +0100

    adress

--- script.js
+++ script.js
@@ -2,54 +2,54 @@ $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $('.navbar').addClass('sticky');
     } else {
-      $(".navbar").removeClass("sticky");
+      $('.navbar').removeClass('sticky');
     }
 
     // scroll-up button show/hide script
     if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
+      $('.scroll-up-btn').addClass('show');
     } else {
-      $(".scroll-up-btn").removeClass("show");
+      $('.scroll-up-btn').removeClass('show');
     }
   });
 
   // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
     // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
+    $('html').css('scrollBehavior', 'auto');
   });
 
-  $(".navbar .menu li a").click(function () {
+  $('.navbar .menu li a').click(function () {
     // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
+    $('html').css('scrollBehavior', 'smooth');
   });
 
   // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
   });
 
   // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Student", "Developer", "Designer"],
+  var typed = new Typed('.typing', {
+    strings: ['Student', 'Developer', 'Designer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Student", "Developer", "Designer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+  // var typed = new Typed(".typing-2", {
+  //   strings: ["Student", "Developer", "Designer"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true,
+  // });
 
   // owl carousel script
-  $(".carousel").owlCarousel({
+  $('.carousel').owlCarousel({
     margin: 20,
     loop: true,
     autoplayTimeOut: 2000,
@@ -72,30 +72,30 @@ $(document).ready(function () {
 });
 
 // Portfolio new js
-const filter_btns = document.querySelectorAll(".filter-btn");
-filter_btns.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    filter_btns.forEach((button) => button.classList.remove("active"));
-    btn.classList.add("active");
+const filter_btns = document.querySelectorAll('.filter-btn');
+filter_btns.forEach(btn =>
+  btn.addEventListener('click', () => {
+    filter_btns.forEach(button => button.classList.remove('active'));
+    btn.classList.add('active');
 
     let filterValue = btn.dataset.filter;
 
-    $(".grid").isotope({ filter: filterValue });
+    $('.grid').isotope({ filter: filterValue });
   })
 );
-$(".grid").isotope({
-  itemSelector: ".grid-item",
-  layoutMode: "fitRows",
-  transitionDuration: "0.6s",
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows',
+  transitionDuration: '0.6s',
 });
 
 // footer new Js
-const footer_input = document.querySelector(".footer-input");
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
+const footer_input = document.querySelector('.footer-input');
+footer_input.addEventListener('focus', () => {
+  footer_input.classList.add('focus');
 });
 
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
+footer_input.addEventListener('blur', () => {
+  if (footer_input.value != '') return;
+  footer_input.classList.remove('focus');
 });
